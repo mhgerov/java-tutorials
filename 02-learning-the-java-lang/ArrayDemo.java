0ef7e460cfa32b43d3ea706cd5353458c999aaaa
Michael Gerov <mhgerov@gmail.com>
2018-06-11 12:18:08 -0400

    arraydemo

---
+++ 02-learning-the-java-lang/ArrayDemo.java
@@ -0,0 +1,11 @@
+class ArrayDemo {
+	public static void main(String[] args) {
+		//declare array of ints
+		int [] anArray;
+
+		anArray = new int[10];
+
+		anArray[0] = 100;
+		System.out.println("Element 0: "+anArray[0]);
+	}
+}
